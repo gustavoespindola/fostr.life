c6c6aad022d1146dde7fcc928778bea5c4b3bde2
gustavoespindola <espindolage@gmail.com>
2017-08-12 20:02:53 -0400

    update

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,7 @@
 $('body').show();
+$(document).on('click', '.navbar a, .button.cta', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - $('.navbar').height() -60
+    }, 500);
+});
